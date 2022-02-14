f6edcd4a3038157f46ea78054384e1fb9f092e62
Arkadiy Stepanov <arkadiy93@hotmail.com>
2022-02-14 12:45:25 +0100

    push updated code

--- Project/IMAGE_PREPARATION/Image_Resizing.py
+++ Project/IMAGE_PREPARATION/Image_Resizing.py
@@ -1,14 +1,13 @@
 import os
-import shutil
-from pathlib import Path
 from PIL import Image
 
+datasets_path = os.path.join(os.getcwd(), os.pardir, os.pardir, "Datasets")
 
-def resize_composite_images_(path, max_size,filetype):
+
+def resize_composite_images(path, max_size, filetype):
     subfolder_list = [f.path for f in os.scandir(path) if f.is_dir()]
     for folder in subfolder_list:
         resize_image(max_size, folder, filetype)
-
 
 
 def resize_image(max_size, folder, filetype):
@@ -20,18 +19,17 @@ def resize_image(max_size, folder, filetype):
             temp_image = Image.open(filename)
             width, height = temp_image.size
             if width >= height and width > max_size:
-                factor = width/max_size
+                factor = width / max_size
                 new_width = int(width / factor)
                 new_height = int(height // factor)
             else:
-                factor = height/max_size
-                new_width = int(width /factor)
+                factor = height / max_size
+                new_width = int(width / factor)
                 new_height = int(height / factor)
-            img = temp_image.resize((new_width,new_height))
-            img.save(os.path.join(folder,'ID_' + dataset_id + '_composite_resized.' + filetype))
-
-
+            img = temp_image.resize((new_width, new_height))
+            img.save(os.path.join(folder, 'ID_' + dataset_id + '_composite_resized.' + filetype))
 
 
 if __name__ == '__main__':
-    resize_composite_images_(os.path.join("c:\\", "Bachelor Oppgave", "Datasets", "Fortresses Iran-Pakistan"), 1024, 'png')
+    images_path = os.path.join(datasets_path, "Fortresses_Uzbekistan")
+    resize_composite_images(images_path, 1024, 'png')
---
+++ Project/Utils.py
@@ -0,0 +1,79 @@
+import openpyxl
+import os
+import json
+
+datasets_path = os.path.join(os.getcwd(), os.pardir, "Datasets")
+source_file = os.path.join(datasets_path, "coords.txt")
+target_file = os.path.join(datasets_path, "Fortress_Datasets_Uzbekistan.xlsx")
+json_file = os.path.join(datasets_path, "2022-02-13T004540.200.json")
+json_file2 = os.path.join(datasets_path, "2022-02-13T010907.200.json")
+country = "Uzbekistan"
+L = 0.001
+
+
+def format(num):
+    return round(num, 5)
+
+
+def writeToTarget(data):
+    workbook = openpyxl.load_workbook(target_file)
+    sheet = workbook.active
+    sheet.delete_rows(2, sheet.max_row)
+
+    for data_entry in data:
+        lat, long = data_entry
+
+        longitude_top_left = long - L
+        longitude_bottom_right = long + L
+        latitude_bottom_right = lat - L
+        latitude_top_left = lat + L
+        sheet.append([
+            format(longitude_top_left),
+            format(latitude_top_left),
+            format(longitude_bottom_right),
+            format(latitude_bottom_right),
+            country
+        ])
+
+    workbook.save(target_file)
+
+
+def create_list_from_text_file():
+    source = os.path.join(source_file)
+    file = open(source, "r")
+    content = file.read()
+    lines = content.split("\n")
+    data = []
+    for line in lines:
+        if not line.strip():
+            continue
+        lat, long = line.split(",")
+        data.append((float(lat), float(long)))
+
+    writeToTarget(data)
+
+
+def load_json():
+    forts = []
+    data = open(json_file)
+    content = json.load(data)
+    for entry in content.get('features'):
+        props = entry.get('properties')
+        if props.get('kind') == 'fort':
+            forts.append(entry)
+
+    coordinates = [fort.get('geometry').get('coordinates') for fort in forts]
+    return coordinates[:20]
+
+def create_source_file(coords):
+    source = os.path.join(source_file)
+    with open(source, 'w') as file:
+        for lang, long in coords:
+            file.write(",".join([str(lang), str(long)]))
+            file.write("\n")
+
+
+if __name__ == "__main__":
+    coords = load_json()
+    create_source_file(coords)
+    create_list_from_text_file()
